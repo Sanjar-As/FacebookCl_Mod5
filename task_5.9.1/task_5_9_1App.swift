5a206ce80797b10e042d435f704855818ad51b60
Sanjar <aslonovsanjar02@gmail.com>
2022-03-03 11:03:34 +0500

    Initial Commit

---
+++ task_5.9.1/task_5_9_1App.swift
@@ -0,0 +1,20 @@
+//
+//  task_5_9_1App.swift
+//  task_5.9.1
+//
+//  Created by Sanjar Aslonov on 03/03/22.
+//
+
+import SwiftUI
+
+@main
+struct task_5_9_1App: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
